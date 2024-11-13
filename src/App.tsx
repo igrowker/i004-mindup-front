a3c4288f6522affc54d7ec3d2f707d580c55d0b2
Falekin7 <falekinldg@gmail.com>
2024-11-13 11:08:58 -0300

    global-styles-config

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
-    <>
-      <p>main</p>
-    </>
-  )
+    <div className="max-w-mobile min-w-mobile mx-auto min-h-screen bg-blue-500">
+      <p className="text-center text-xl text-white">main</p>
+      <p className="text-center text-lg text-white">probado probandp</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      'mobile-min': '375px',
+      'mobile-max': '576px',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
+      maxWidth: {
+        mobile: '576px',
+      },
+      minWidth: {
+        mobile: '375px',
+      },
+      colors: {
+        primary: '#4A90E2',
+        secondary: '#7A5FE7',
+        background: '#FFFFFF',
+        text: '#4A4A4A',
+        accent: '#7ED321',
+      },
+    },
   },
   plugins: [],
-}
+};
