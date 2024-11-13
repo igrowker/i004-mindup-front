66fff4ff9c673971678ac88991781f4ad2fc7b60
Camilo Rodriguez <camiloceatrabajo@gmail.com>
2024-11-13 17:04:07 -0300

    Maquetado inicial del perfil profesional

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css';
+import Profile from './pages/profile';
 
 function App() {
   return (
-    <div className="max-w-mobile min-w-mobile mx-auto min-h-screen bg-blue-500">
-      <p className="text-center text-xl text-white">main</p>
-      <p className="text-center text-lg text-white">probado probandp</p>
+    <div className="min-h-screen bg-background">
+      <Profile/>
     </div>
   );
 }
