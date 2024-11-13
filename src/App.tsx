170f21eaafa5f8bdfe8980e86a53201fb01274b1
LudwingValecillos <ludwingvaldev@gmail.com>
2024-11-13 17:36:53 -0300

    correcion del registro

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import RegisterForm from "./pages/RegisterForm";
 
 function App() {
   return (
     <div className="max-w-mobile min-w-mobile mx-auto min-h-screen">
-      <p className="text-center text-xl text-white">main</p>
-      <p className="text-center text-lg text-white">probado probandp</p>
+      {/* <p className="text-center text-xl text-white">main</p>
+      <p className="text-center text-lg text-white">probado probandp</p> */}
+      <RegisterForm />
     </div>
   );
 }
--- src/components/shared/CustomButton.tsx
+++ src/components/shared/CustomButton.tsx
@@ -7,33 +7,22 @@ interface ButtonData {
   onClick?: () => void;
 }
 
-function CustomButton({ title, type, appearance, onClick }: ButtonData): JSX.Element {
+function CustomButton({ title, appearance, onClick }: ButtonData): JSX.Element {
+  const commonClasses = "inline-block text-center rounded border px-24 py-3 font-medium focus:outline-none focus:ring";
+  const primaryClasses = "border-[#7a5fe7] bg-[#7a5fe7] text-white hover:bg-transparent hover:text-[#7a5fe7] active:text-[#7a5fe7]";
+  const secondaryClasses = "border-[#7a5fe7] text-[#7a5fe7] hover:bg-[#7a5fe7] hover:text-white active:bg-[#7a5fe7]";
+
   return (
-    <>
-      {appearance == true ? (
-        <a
-          className="inline-block text-center rounded border border-[#7a5fe7] bg-[#7a5fe7] px-24 py-3 min-w-56 font-medium text-white hover:bg-transparent hover:text-[#7a5fe7] focus:outline-none focus:ring active:text-[#7a5fe7]"
-          href="#"
-          onClick={(e) => {
-            e.preventDefault(); // Evita que el enlace navegue
-            if (onClick) onClick(); // Ejecuta la función onClick si está presente
-          }}
-        >
-          {title}
-        </a>
-      ) : (
-        <a
-          className="inline-block text-center rounded border border-[[#7a5fe7]] px-24 py-3 font-medium text-[#7a5fe7] hover:bg-[#7a5fe7] hover:text-white focus:outline-none focus:ring active:bg-[#7a5fe7]"
-          href="#"
-          onClick={(e) => {
-            e.preventDefault(); // Evita que el enlace navegue
-            if (onClick) onClick(); // Ejecuta la función onClick si está presente
-          }}
-        >
-          {title}
-        </a>
-      )}
-    </>
+    <a
+      className={`${commonClasses} ${appearance ? primaryClasses : secondaryClasses}`}
+      href="#"
+      onClick={(e) => {
+        e.preventDefault(); // Evita que el enlace navegue
+        if (onClick) onClick(); // Ejecuta la función onClick si está presente
+      }}
+    >
+      {title}
+    </a>
   );
 }
 
