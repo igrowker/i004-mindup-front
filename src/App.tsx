2fea4b50871aa820cfaa390e38a483c6e9c520a2
Falekin7 <falekinldg@gmail.com>
2024-11-13 21:21:47 -0300

    onboardin component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Profile from './pages/profile';
-import RegisterForm from "./pages/RegisterForm";
+import RegisterForm from './pages/RegisterForm';
+import Onboarding from './pages/Onboarding';
 
 function App() {
   return (
     <>
       <Profile />
       <RegisterForm />
+      <Onboarding />
     </>
   );
 }
