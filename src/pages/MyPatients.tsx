9f67f7784aa2e0d2cf9fe41637c418deacd77171
kevin joel noviello <kjnoviello@gmail.com>
2024-11-22 21:55:13 -0300

    minor

--- src/pages/MyPatients.tsx
+++ src/pages/MyPatients.tsx
@@ -1,5 +1,5 @@
 import Header from '../components/header/Header'
-import PatientCard from '../components/mypatients/patientCard'
+import PatientCard from '../components/mypatients/PatientCard';
 
 const MyPatients = () => {
 
