2e10320623463b1962473321416be5a24a608759
Camilo Rodriguez <camiloceatrabajo@gmail.com>
2024-11-22 21:05:10 -0300

    Todo el enrutado y el flow de la aplicacion dependiendo de si es profesional o paciente, tambien modificaciones al nav para lo mismo

--- src/api/userService.ts
+++ src/api/userService.ts
@@ -1,4 +1,4 @@
-const URL = 'https://673d45350118dbfe8606b241.mockapi.io/users/pacient';
+const URL = 'https://673d45350118dbfe8606b241.mockapi.io/users/pacient'; //Maquetado devolviendo dos usuarios, por eso modifique para que devuelva dos usuarios
 
 // funcion que hace la solicitud http para obtener el user
 export const fetchUser = async () => {
@@ -12,9 +12,9 @@ export const fetchUser = async () => {
       throw new Error(`HTTP error! status: ${res.status}`);
     }
 
-    const user = await res.json();
-    console.log(user);
-    return user;
+    const users = await res.json();
+    console.log(users);
+    return users;
   } catch (error) {
     console.error('Error fetching user:', error);
   }
